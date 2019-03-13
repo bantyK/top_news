5b65fb1b9720d35239306ddebe32991629dedaa3
bantyK <bantyp92@gmail.com>
2019-03-13 08:14:11 +0530

    1. Webview added 2. Final adjustments to recycler view item

---
+++ TopNews/app/src/main/java/com/banty/topnews/ui/activities/webview/WebviewActivity.kt
@@ -0,0 +1,13 @@
+package com.banty.topnews.ui.activities.webview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.banty.topnews.R
+
+class WebviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+    }
+}
